79b597e631f82f7579cc38b0abb0a870ca8fa802
Paul Clark <clark.paul123@gmail.com>
2018-10-21 16:55:58 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,6 @@ def first_challenge
   }
 
   contacts.each do |person, data|
-    if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           value.delete_if do {|flavour| flavour == "strawberry"}
